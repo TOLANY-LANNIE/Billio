9d40e0dc474c054ab9fcb4cfbee0913f632cc8f7
Thulani Mpofu <tolanym_2@live.com>
2023-05-20 23:02:01 +0200

    bills fragment viewmodel and factory added

---
+++ app/src/main/java/com/thulani/billio/fragments/bills/BillsFragment.kt
@@ -0,0 +1,20 @@
+package com.thulani.billio.fragments.bills
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.thulani.billio.R
+
+class BillsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bills, container, false)
+    }
+}
