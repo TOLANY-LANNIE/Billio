65fe40522cc9e41b94fa7eee68b992b01748604c
Thulani Mpofu <tolanym_2@live.com>
2023-05-23 22:12:19 +0200

    CategoriesViewModel.kt and CategoriesViewModelFactory.kt completed

--- app/src/main/java/com/thulani/billio/fragments/categories/CategoriesViewModel.kt
+++ app/src/main/java/com/thulani/billio/fragments/categories/CategoriesViewModel.kt
@@ -1,4 +1,29 @@
 package com.thulani.billio.fragments.categories
 
-class CategoriesViewModel {
+import androidx.lifecycle.ViewModel
+import com.thulani.billio.data.entities.Categories
+import com.thulani.billio.data.repository.CategoryRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class CategoriesViewModel(
+    private val repository: CategoryRepository
+):ViewModel(){
+    val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    fun upsert(category: Categories) = coroutineScope.launch {
+        repository.upsert(category)
+    }
+    fun delete(category: Categories) = coroutineScope.launch {
+        repository.delete(category)
+    }
+
+    fun getAllCategories() = repository.getAllCategories()
+
+    fun selectedCategory(categoryName:String){
+        coroutineScope.launch {
+            val category = repository.selectedCategory(categoryName)
+        }
+    }
 }
--- app/src/main/java/com/thulani/billio/fragments/categories/CategoriesViewModelFactory.kt
+++ app/src/main/java/com/thulani/billio/fragments/categories/CategoriesViewModelFactory.kt
@@ -1,4 +1,17 @@
 package com.thulani.billio.fragments.categories
 
-class CategoriesViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.thulani.billio.data.repository.CategoryRepository
+import com.thulani.billio.data.repository.UserRepository
+import com.thulani.billio.fragments.auth.UserViewModel
+
+@Suppress("UNCHECKED_CAST")
+class CategoriesViewModelFactory(
+    private val repository: CategoryRepository
+):ViewModelProvider.NewInstanceFactory(){
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        return CategoriesViewModel(repository) as T
+    }
 }
