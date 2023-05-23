3b4fccc4296ff089280e43ab37db478a1f7b3d1a
Thulani Mpofu <tolanym_2@live.com>
2023-05-23 21:58:48 +0200

    CategoryRepository.kt created

--- app/src/main/java/com/thulani/billio/data/repository/CategoryRepository.kt
+++ app/src/main/java/com/thulani/billio/data/repository/CategoryRepository.kt
@@ -1,4 +1,20 @@
 package com.thulani.billio.data.repository
 
-class CategoryRepository {
+import com.thulani.billio.data.BillioDB
+import com.thulani.billio.data.entities.Categories
+
+class CategoryRepository(
+    private val database:BillioDB
+    ) {
+
+    suspend fun upsert(categories: Categories) = database.getCategoriesDao().upsert(categories)
+
+    suspend fun delete(categories: Categories)= database.getCategoriesDao().delete(categories)
+
+    fun getAllCategories() = database.getCategoriesDao().getAll()
+
+
+    suspend fun selectedCategory(categoryName:String):Categories?{
+        return database.getCategoriesDao().getSelectedCat(categoryName)
+    }
 }
--- app/src/main/java/com/thulani/billio/fragments/categories/CategoriesFragment.kt
+++ app/src/main/java/com/thulani/billio/fragments/categories/CategoriesFragment.kt
@@ -6,14 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.thulani.billio.R
+import com.thulani.billio.databinding.FragmentCategoriesBinding
 
 class CategoriesFragment : Fragment() {
+    private var _binding: FragmentCategoriesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_categories, container, false)
+        _binding = FragmentCategoriesBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
