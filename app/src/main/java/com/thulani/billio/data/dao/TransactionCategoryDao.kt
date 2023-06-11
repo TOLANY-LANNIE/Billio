0e4869654c02f3ca041400e5c80cc239aec48f38
Thulani Mpofu <tolanym_2@live.com>
2023-06-11 12:43:37 +0200

    update to TransactionCategoryDao.kt and TransactionUserDao.kt

--- app/src/main/java/com/thulani/billio/data/dao/TransactionCategoryDao.kt
+++ app/src/main/java/com/thulani/billio/data/dao/TransactionCategoryDao.kt
@@ -1,4 +1,14 @@
 package com.thulani.billio.data.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import com.thulani.billio.data.entities.Categories
+import com.thulani.billio.data.entities.Transactions
+
+
+@Dao
 interface TransactionCategoryDao {
+    @Query("SELECT * FROM transaction_table "+
+            "JOIN categories_table ON transaction_table.categoryID = categories_table.cid")
+    fun loadTransactionByCategoryNames(): Map<Transactions, List<Categories>>
 }
--- app/src/main/java/com/thulani/billio/data/dao/TransactionUserDao.kt
+++ app/src/main/java/com/thulani/billio/data/dao/TransactionUserDao.kt
@@ -1,4 +1,15 @@
 package com.thulani.billio.data.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import com.thulani.billio.data.entities.Transactions
+
+import com.thulani.billio.data.entities.User
+
+
+@Dao
 interface TransactionUserDao {
+    @Query("SELECT * FROM users_table "+
+            "JOIN transaction_table ON users_table.uid = transaction_table.user_id")
+    fun loadUserAndTransactionNames(): Map<User, List<Transactions>>
 }
