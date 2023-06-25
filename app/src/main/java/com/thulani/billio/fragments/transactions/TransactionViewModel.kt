dba0607aaf231a81d5d1f39a3e4bda08d2b97f42
Thulani Mpofu <tolanym_2@live.com>
2023-06-25 16:37:56 +0200

    TransactionViewModel.kt and TransactionViewModelFactory.kt completed

--- app/src/main/java/com/thulani/billio/fragments/transactions/TransactionViewModel.kt
+++ app/src/main/java/com/thulani/billio/fragments/transactions/TransactionViewModel.kt
@@ -1,4 +1,32 @@
 package com.thulani.billio.fragments.transactions
 
-class TransactionViewModel {
+import androidx.lifecycle.ViewModel
+import com.thulani.billio.data.entities.Transactions
+import com.thulani.billio.data.repository.TransactionRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TransactionViewModel(
+    private val repository: TransactionRepository
+):ViewModel() {
+    val coroutineScope = CoroutineScope(Dispatchers.IO)
+
+    fun upsert(transactions: Transactions) =coroutineScope.launch {
+        repository.upsert(transactions)
+    }
+
+    fun delete(transactions: Transactions)=coroutineScope.launch {
+        repository.delete(transactions)
+    }
+
+    fun getAllTransactions() =repository.getAllTransactions()
+
+    fun getAllTransactionsByCategory()=repository.getAllTransactionsByCategory()
+
+    fun getAllTransactionsByUser() =repository.getAllTransactionByUser()
+
+    fun searchTransaction(transactionName:String) =coroutineScope.launch {
+        repository.searchTransactions(transactionName)
+    }
 }
--- app/src/main/java/com/thulani/billio/fragments/transactions/TransactionViewModelFactory.kt
+++ app/src/main/java/com/thulani/billio/fragments/transactions/TransactionViewModelFactory.kt
@@ -1,6 +1,15 @@
 package com.thulani.billio.fragments.transactions
 
-class TransactionViewModelFactory(
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.thulani.billio.data.repository.TransactionRepository
+import com.thulani.billio.fragments.categories.CategoriesViewModel
 
-) {
+class TransactionViewModelFactory(
+    private val repository:TransactionRepository
+):ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        return TransactionViewModel(repository) as T
+    }
 }
